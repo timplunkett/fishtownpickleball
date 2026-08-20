f2f12baf98e4fb3412f29e4bb5d6c96a4fd95c2a
Tim Plunkett <git@plnktt.com>
2026-08-20 17:28:35 -0400

    chore(CPL): Add gendered travel leagues

--- cpl/travel/bootstrap.js
+++ cpl/travel/bootstrap.js
@@ -8,7 +8,10 @@
     { slug: 'c43b8608', divisionName: '3.5 (50+)' },
     { slug: 'a1413f3d', divisionName: '4.0' },
     { slug: '2edc44e7', divisionName: '4.0 (50+)' },
-    { slug: '6619816f', divisionName: '4.5' }
+    { slug: '6619816f', divisionName: '4.5' },
+    { slug: 'ad44e3bd', divisionName: '3.25 Womens' },
+    { slug: 'c118b8e9', divisionName: '3.75 Mens' },
+    { slug: 'cca69ab9', divisionName: '4.5 Mens' }
   ]);
   const CONFIG = Object.freeze({
     dashboardPath: '/cpl/travel',
